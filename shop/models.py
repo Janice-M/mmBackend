e77bf311a364e78f89c765549befccc3c6f386b6
Janice-M <janiceink001@gmail.com>
2019-12-09 13:21:10 +0300

    added the Customer model for sign up

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Customer(models.Model):
+    firstName = models.CharField(max_length =30)
+    lastName  = models.CharField(max_length =30)
+    email = models.EmailField()
