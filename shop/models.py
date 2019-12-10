e48529d4312c1bc3c633dab7e1bc5a115b187b46
Janice-M <janiceink001@gmail.com>
2019-12-10 13:09:29 +0300

    customer

--- shop/models.py
+++ shop/models.py
@@ -1,9 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
 
 
 class Customer(models.Model):
-    firstName = models.CharField(max_length =30)
-    lastName  = models.CharField(max_length =30)
+    userName = models.CharField(max_length =30)
     email = models.EmailField()
+    
+    objects = models.Manager()
+
+    def save_customer(self):
+        self.save()
+        
+    @classmethod
+    def get_profile(cls, id):
+        profile = Customer.objects.get(user=id)
+        return Customer
