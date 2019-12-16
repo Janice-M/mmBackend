70859edef3acfb8bb3c4520a6a36a86cfd09e4ee
Janice-M <janiceink001@gmail.com>
2019-12-16 16:44:34 +0300

    linking shop in shop setting up configs#

--- shop/urls.py
+++ shop/urls.py
@@ -3,3 +3,7 @@ from django.conf.urls.static import static
 from django.conf.urls import url
 from . import views
 ''' end of importations '''
+
+urlpatterns = [
+    path('',views.index,name='index'),
+]
