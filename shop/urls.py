fc3f506efa2f05eceaab5b1a6543e256bc442223
Janice-M <janiceink001@gmail.com>
2019-12-16 16:49:04 +0300

    linking shop in shop setting up configs

--- shop/urls.py
+++ shop/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import url
+from django.urls import path
 from . import views
 ''' end of importations '''
 
