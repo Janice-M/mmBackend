2a0461bef2db90823ffd03ccaa4ed3132fcb7257
Janice-M <janiceink001@gmail.com>
2019-12-19 15:33:10 +0300

    django urls include

--- shop/urls.py
+++ shop/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import url
-from django.urls import path
+from django.urls import path, include
 from . import views
 ''' end of importations '''
 
