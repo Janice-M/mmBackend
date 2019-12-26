e41b9f272196fbe037853c7f7b7c0af12f2f2770
Janice-M <janiceink001@gmail.com>
2019-12-26 20:20:35 +0300

    admin registering service

--- shop/admin.py
+++ shop/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Customer,Category, Product
+from .models import Customer,Category, Product,Service
 # Register your models here.
 
 
 admin.site.register(Customer)
 admin.site.register(Category)
 admin.site.register(Product)
+admin.site.register(Service)
+
