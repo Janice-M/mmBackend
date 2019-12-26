16539ee548bcc766d74510a37568eeab896904ed
Janice-M <janiceink001@gmail.com>
2019-12-26 20:12:03 +0300

    created a service class without category field because services are fewer

--- shop/models.py
+++ shop/models.py
@@ -44,14 +44,14 @@ class Product(models.model):
         
 
         
-    class Service(models.model):
-        mainimage=models.ImageField(upload_to='products/',blank=True)
-        name= models.CharField(max_length=300)
-        slug = models.SlugField()
-        preview_text = models.TextField(max_length=200, verbose_name = 'Preview Text')
-        detail_text = models.TextField(max_length=1000, verbose_name ='Detail Text')
-        price = models.FloatField()
+class Service(models.model):
+    mainimage=models.ImageField(upload_to='products/',blank=True)
+    name= models.CharField(max_length=300)
+    slug = models.SlugField()
+    preview_text = models.TextField(max_length=200, verbose_name = 'Preview Text')
+    detail_text = models.TextField(max_length=1000, verbose_name ='Detail Text')
+    price = models.FloatField()
         
         
-        def__str__(self):
-            return self.name
+    def__str__(self):
+        return self.name
