84f894856d7df8a626325e8bca363fb28dabfce6
Janice-M <janiceink001@gmail.com>
2019-12-30 08:08:03 +0300

    add to cart views

--- shop/urls.py
+++ shop/urls.py
@@ -2,8 +2,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import url
 from django.urls import path, include
-from . import views
+from . views import Home
+
+
 ''' end of importations '''
+#app name
+app_name='mainapp'
+
 
 urlpatterns = [
     path('',Home.as_view().name='home'),
