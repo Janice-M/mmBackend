9b534dddaf1e658d9113b801d401578f7095ae84
Janice-M <janiceink001@gmail.com>
2019-12-30 08:00:32 +0300

    add to cartas a second application for reusability

--- cart/models.py
+++ cart/models.py
@@ -10,4 +10,22 @@ Customer=get_customer_model()
 #Cart
 
 class Cart(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    item = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    created = models.DateTimeField(auto_now_add=True)
     
+    def __str__(self):
+        return f'{self.quantity} of {self.item.name}'
+    
+    
+#ordering after adding to cart model
+
+class Order(models.Model):
+    orderItems  = models.ManyToManyField(Cart)
+    Customer = models.ForeignKey(User, on_delete=models.CASCADE)
+    ordered = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username
--- shop/models.py
+++ shop/models.py
@@ -4,19 +4,6 @@ from django.db import models
 # Create your models here.
 
 
-class Customer(models.Model):
-    userName = models.CharField(max_length =30)
-    email = models.EmailField()
-    
-    objects = models.Manager()
-
-    def save_customer(self):
-        self.save()
-        
-    @classmethod
-    def get_customer(cls, id):
-        customer = Customer.objects.get(user=id)
-        return customer
     
 #category model
     
@@ -24,7 +11,7 @@ class Category (models.Model):
     title = models.CharField(max_length=300)
     primaryCategory= models.BooleanField(default=False)
     
-    def__str__(self):
+    def__str__(self)
         return self.title
     
 #Product Model
