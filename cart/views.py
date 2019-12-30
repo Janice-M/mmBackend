621b12d8c6ff2af7bd4981683363d79893777707
Janice-M <janiceink001@gmail.com>
2019-12-30 09:19:19 +0300

    view for removing things from cart

--- cart/views.py
+++ cart/views.py
@@ -16,7 +16,7 @@ def add_to_cart(request, slug):
         if order.orderitems.filter(item__slug=item.slug).exists():
             order_item.quantity += 1
             order_item.save()
-            messages.info(request, "This item quantity was updated.")
+            messages.info(request, "This car part was updated.")
             return redirect("mainapp:home")
         else:
             order.orderitems.add(order_item)
@@ -28,3 +28,43 @@ def add_to_cart(request, slug):
         order.orderitems.add(order_item)
         messages.info(request, "This item was added to your cart.")
         return redirect("mainapp:home")
+    
+    
+    
+    
+    
+#Removing item from the cart
+def remove_from_cart(request, slug):
+    item = get_object_or_404(Product, slug=slug)
+    cart_qs = Cart.objects.filter(user=request.user, item=item)
+    if cart_qs.exists():
+        cart = cart_qs[0]
+        # Checking the cart quantity
+        if cart.quantity > 1:
+            cart.quantity -= 1
+            cart.save()
+        else:
+            cart_qs.delete()
+    order_qs = Order.objects.filter(
+        user=request.user,
+        ordered=False
+    )
+    if order_qs.exists():
+        order = order_qs[0]
+        # check if the order item is in the order
+        if order.orderitems.filter(item__slug=item.slug).exists():
+            order_item = Cart.objects.filter(
+                item=item,
+                user=request.user,
+            )[0]
+            order.orderitems.remove(order_item)
+            messages.info(request, "This item was removed from your cart.")
+            return redirect("mainapp:home")
+        else:
+            messages.info(request, "This item was not in your cart")
+            return redirect("mainapp:home")
+    else:
+        messages.info(request, "You do not have an active order")
+        return redirect("core:home")
+
+
