c8b495c9984020f16f405e59f9fd4915f0f6c402
Janice-M <janiceink001@gmail.com>
2019-12-30 07:53:09 +0300

    add to cartas a second application for reusability

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from shop.models import *
 # Create your models here.
+
+
+#Get the customer model
+
+Customer=get_customer_model()
+
+#Cart
+
+class Cart(models.Model):
+    
