f070026773dda72418578d5de3d89bada50dd8c3
Janice-M <janiceink001@gmail.com>
2020-01-02 23:39:08 +0300

    adding register template for navbar

--- cart/templatetags/cart_tag.py
+++ cart/templatetags/cart_tag.py
@@ -6,3 +6,7 @@ register = template.Library()
 @register.filter
 def cart_total(user):
     order = Order.objects.filter(user=user, ordered=False)
+     if order.exists():
+        	return order[0].orderitems.count()
+    else:
+    	return 0
