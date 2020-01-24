bb36a25c0e88718a85e5b9c20e4b5405f3824df2
Janice-M <janiceink001@gmail.com>
2020-01-24 13:26:05 +0300

    serializer

--- shop/serializers.py
+++ shop/serializers.py
@@ -1,4 +1,16 @@
 from rest_framework import serializers
 from .models import Category, Product, Service
-
-class studentSerializer(
+class studentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Students
+        fields = '__all__'
+class studentSerializer(serializers.ModelSerializer):
+        class Meta:
+        model = Students
+        fields = '__all__'     
+class studentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Students
+        fields = '__all__'
+        
+        
