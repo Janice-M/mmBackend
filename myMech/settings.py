4027a6a314749c40db81d3581b3370b278a804db
Janice-M <janiceink001@gmail.com>
2020-02-01 12:43:43 +0300

    migrations smooth

--- myMech/settings.py
+++ myMech/settings.py
@@ -78,8 +78,10 @@ WSGI_APPLICATION = 'myMech.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR,'newdb3.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mech',
+        'USER':'moringa',
+        'PASSWORD':'**kwasync'
     }
 }
 
