ca7210efbe3861600a153e6de6942c2cd8569652
Janice-M <janiceink001@gmail.com>
2020-02-01 11:44:52 +0300

    migrations smooth

--- myMech/settings.py
+++ myMech/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'cart',
+  
     'shop',
     'bootstrap4',
     'django.contrib.admin',
--- shop/urls.py
+++ shop/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.static import static
 from django.conf.urls import url
 from django.urls import path, include
 from . views import Home
-from cart.views import add_to_cart, remove_from_cart
+
 
 
 ''' end of importations '''
@@ -13,6 +13,5 @@ app_name='mainapp'
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
-    path('cart/<slug>',add_to_cart, name='cart'),
-    path('remove/<slug>', remove_from_cart, name='remove-cart'),
+    
 ]
