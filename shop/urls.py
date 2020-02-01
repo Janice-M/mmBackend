cd32f91cf460079505b84dedd12262c9d4905a15
Janice-M <janiceink001@gmail.com>
2020-02-01 13:01:31 +0300

    endpoints

--- shop/urls.py
+++ shop/urls.py
@@ -2,18 +2,15 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import url
 from django.urls import path, include
-from . views import Home,Service
+from . views import ProductView,ServiceView
 # from cart.views import add_to_cart, remove_from_cart
 
 
 ''' end of importations '''
 #app name
-app_name='mainapp'
+app_name='products'
 
 
 urlpatterns = [
-    path('', Home.as_view(), name='home'),
-    path('services/',Service.as_view(), name="service"),
-    # path('cart/<slug>',add_to_cart, name='cart'),
-    # path('remove/<slug>', remove_from_cart, name='remove-cart'),
+    path('products/', ProductView.as_view()),
 ]
