718f6ec5b8de6de3e5b77fc4790c777e8248234a
Janice-M <janiceink001@gmail.com>
2020-02-06 23:43:09 +0300

    title

--- shop/serializers.py
+++ shop/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Category, Product, Service
 
-#model serializers 
+#model
 
 class categorySerializer(serializers.ModelSerializer):
     class Meta:
