83eba4462a7c0f045f14566572f2dd14de1fbbc5
Janice-M <janiceink001@gmail.com>
2020-02-10 23:29:56 +0300

    car migrated

--- shop/serializers.py
+++ shop/serializers.py
@@ -14,10 +14,7 @@ class ProductSerializer(serializers.ModelSerializer):
             name= serializers.CharField(max_length=300)
             slug = serializers.SlugField()
             
-            preview_text = serializers.CharField(max_length=200, verbose_name = 'Preview Text')
-            detail_text = serializers.CharField(max_length=1000, verbose_name ='Detail Text')
-            price = serialisers.CharField() 
-            user_id = serializers.CharField()
+            
         
             def create(self, validated_data):
                 return Product.objects.create(**validated_data)    
