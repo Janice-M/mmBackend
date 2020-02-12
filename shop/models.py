22db4506fda214a450a1190d3ea4ae58f861dff5
Janice-M <janiceink001@gmail.com>
2020-02-12 23:36:50 +0300

    three api fields showing

--- shop/models.py
+++ shop/models.py
@@ -20,7 +20,7 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     preview_text = models.TextField(max_length=200, verbose_name = 'Preview Text')
     detail_text = models.TextField(max_length=1000, verbose_name ='Detail Text')
-    price = models.FloatField()
+    price = models.FloatFi
         
         
     def __str__(self):
