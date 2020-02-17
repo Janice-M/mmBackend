4959e30a5de878e8604b1c35d58f67aad187e2d0
Janice-M <janiceink001@gmail.com>
2020-02-17 16:39:55 +0300

    tests

--- shop/tests.py
+++ shop/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
+from rest_framework.test import APITestCase
+from django.contrib.auth.models import User
+from rest_framework import status
 
 # Create your tests here.
