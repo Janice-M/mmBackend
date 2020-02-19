af00bfa0e5268c3aaa4ea01e436373e6b9c2a622
Janice-M <janiceink001@gmail.com>
2020-02-19 06:30:21 +0300

    service

--- shop/views.py
+++ shop/views.py
@@ -66,10 +66,26 @@ class ProductView(APIView):
     # get method for all services
     
 class ServiceView(APIView):
+    
     def get(self, request):
         services = Service.objects.all()
         return Response({"services": services})
     
+    def post(self, request):
+            product = request.data.get('product')
+
+        # Create a service on mech from the above data
+            serializer = ServiceSerializer(data=product)
+            if serializer.is_valid(raise_exception=True):
+                product_saved = serializer.save()
+            return Response({"success": "Product '{}' created successfully".format(product_saved.name)})
+    
+    def delete(self, request, pk):
+        # Get object 
+        product = get_object_or_404(Product.objects.all(), pk=pk)
+        pk = self.kwargs.get('pk')
+        product.delete()
+        return Response({"message": "Product with id `{}` has been deleted.".format(pk)},status=204)
 class CarView(APIView):
     def get(self, request):
         services = Car.objects.all()
