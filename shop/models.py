d46bfa579481563fd9b616b1a20bfaf741be09e7
Janice-M <janiceink001@gmail.com>
2020-02-19 06:20:19 +0300

    pk

--- shop/models.py
+++ shop/models.py
@@ -31,7 +31,7 @@ class Product(models.Model):
     
     mainimage=models.ImageField(upload_to='products/',blank=True)
     name= models.CharField(max_length=300)
-    slug = models.SlugField()
+    slug = models.SlugField(primary_key=True, unique=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     preview_text = models.TextField(max_length=200, verbose_name = 'Preview Text')
     detail_text = models.TextField(max_length=1000, verbose_name ='Detail Text')
@@ -62,7 +62,7 @@ class Car(models.Model):
 class Service(models.Model):
     mainimage=models.ImageField(upload_to='services/',blank=True)
     name= models.CharField(max_length=300)
-    slug = models.SlugField()
+    slug = models.SlugField( primary_key= True, unique =True)
     preview_text = models.TextField(max_length=200, verbose_name = 'Preview Text')
     detail_text = models.TextField(max_length=1000, verbose_name ='Detail Text')
     price = models.FloatField()
